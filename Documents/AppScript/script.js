60b2c54f1fc42bb6668d5462671325db2f32ad2b
Oscar van der Westhuixen <oscar.vdw99@gmail.com>
2024-08-05 17:00:56 +0200

    Added new Maps for kay value pairs.

--- Documents/AppScript/script.js
+++ Documents/AppScript/script.js
@@ -8,9 +8,21 @@ let startDate = new Date(2024, 06, 30); //Measured in miliseconds
 let endDate = new Date(2024, 08, 01);
 const sheetName = 'Copy';
 
-//In stead of enum, do key value pairs bruh
-//Use maps - https://www.w3schools.com/js/js_maps.asp
-//Hold onto enums to configure items? No, this can be done in the map.
+//In stead of enum, Use maps - https://www.w3schools.com/js/js_maps.asp
+//Map email to fullName
+const Developers = new Map([
+  ['charles.li@velosure.com.au', 'Charles Li'],
+  ['bjorn@twothreebird.com', 'Björn Theart'],
+  ['vernon@twothreebird.com', 'Vernon Grant'],
+  ['hitesh@twothreebird.com', 'Hitesh Maity'],
+  ['ryan@twothreebird.com', 'Ryan Peel'],
+  ['curtis@twothreebird.com', 'Curtis Page'],
+  ['dirk@twothreebird.com', 'Dirk Dircksen'],
+  ['brendan@twothreebird.com', 'Brendan van der Meulen'],
+  ['sergei@twothreebird.com', 'Sergei Pringiers'],
+  ['vijay@twothreebird.com', 'Vijay Kumar']
+]);
+
 const Devs = {
   CHARLES: 'charles.li@velosure.com.au', //and add a value 'Charles Li'
   CLYDE: 'clyde@twothreebird.com',
@@ -25,8 +37,24 @@ const Devs = {
   VIJAY: 'vijay@twothreebird.com',
 };
 
-//Add switch in separateDevs() for header index
-//Or foreach() enum compares to headers
+//Map dev : correcting factor
+//Handle the logic of adding and storing the correcting factor in a function
+let correctingFactor = new Map([
+  ['Charles Li', 1],
+  ['Björn Theart', 1],
+  ['Vernon Grant', 1],
+  ['Hitesh Maity', 1],
+  ['Ryan Peel', 1],
+  ['Curtis Page', 1],
+  ['Dirk Dircksen', 1],
+  ['Brendan van der Meulen', 1],
+  ['Sergei Pringiers', 1],
+  ['Vijay Kumar', 1]
+]);
+
+//In stead of enum, Use maps - https://www.w3schools.com/js/js_maps.asp
+//Map headerName to index
+//Not const
 const Headers = {
   CREATED: 'Created At',
   COMPLETED: 'Completed At',
@@ -40,24 +68,26 @@ const Headers = {
   ESTTIME: 'Estimated time',
   ROLLTIME: 'Rollover time',
   STDTIME: 'Standardised time',
+  ACTUALTIME: 'Actual time', //Use actual time for the correcting factored time?
   COST: 'Cost'
 }
 
-let HeadersIndex = {
-  CREATED_INDEX: -1,
-  COMPLETED_INDEX: -1,
-  MODIFIED_INDEX: -1,
-  NAME_INDEX: -1,
-  PROGRESS_INDEX: -1,
-  BRAND_INDEX: -1,
-  DEVELOPER_INDEX: -1,
-  CATEGORY_INDEX: -1,
-  REGION_INDEX: -1,
-  ESTTIME_INDEX: -1,
-  ROLLTIME_INDEX: -1,
-  STDTIME_INDEX: -1,
-  COST_INDEX: -1
-}
+let HeadersIndex = new Map([
+  ['Created At', -1],
+  ['Completed At', -1],
+  ['Last Modified', -1],
+  ['Name', -1],
+  ['Tech Progress', -1],
+  ['Brand', -1],
+  ['Dev', -1],
+  ['Tech Category', -1],
+  ['Region', -1],
+  ['Estimated time', -1],
+  ['Rollover time', -1],
+  ['Standardised time', -1],
+  ['Actual time', -1], //Use actual time for the correcting factored time?
+  ['Cost', -1]
+]);
 
 //function setHeaderKeyValuePairs() {}
 
@@ -140,6 +170,7 @@ function separateDev() {
   let brandColIndex = -1;
   let regionColIndex = -1;
   let nameColIndex = -1;
+  let techCatIndex = -1;
   for(let i=0; i<headerRow.length; i++) {
     if(headerRow[i] === 'Dev') {
       Logger.log('devColIndex: '+ (i+1));
@@ -158,6 +189,8 @@ function separateDev() {
       nameColIndex = i+1;
     } else if(headerRow[i] === 'Rollover time') {
       rollHrsIndex = i+1;
+    } else if(headerRow[i] === 'Tech category') {
+      techCatIndex = i+1;
     }
   }
   //Separate devs
@@ -172,6 +205,7 @@ function separateDev() {
       let brandTemp = sheet.getRange(noRows,brandColIndex).getValue();
       let regionTemp = sheet.getRange(noRows,regionColIndex).getValue();
       let nameTemp = sheet.getRange(noRows, nameColIndex).getValue();
+      let techCatTemp = sheet.getRange(noRows, nameColIndex).getValue();
       for(let i=0; i<cellValue.split(',').length-1; i++) {
         sheet.insertRowAfter(noRows);
         sheet.getRange(noRows+1,devColIndex).setValue(splitDevs[i+1]).trimWhitespace();
@@ -180,6 +214,7 @@ function separateDev() {
         sheet.getRange(noRows+1,brandColIndex).setValue(brandTemp);
         sheet.getRange(noRows+1,rollHrsIndex).setValue(rollHrsTemp);
         sheet.getRange(noRows+1,estTimeColIndex).setValue(estTimeTemp);
+        sheet.getRange(noRows+1,techCatIndex).setValue(techCatIndex);
       }
       sheet.getRange(noRows,devColIndex).setValue(splitDevs[0]);
     }
@@ -260,6 +295,7 @@ function differenceCalc() {
   let headerRow = sheet.getRange(1,1,1,sheet.getLastColumn()).getValues()[0];
   for(let i=1; i<headerRow.length; i++) {
     cellValue = sheet.getRange(1,i).getValue();
+    //Logger.log(sheet.getRange(1,i).getA1Notation());
     if(cellValue == Headers.ESTTIME) {
       estIndex = i;
       Logger.log('est time: ' + sheet.getRange(1,i).getA1Notation() + ' cellValue: ' + sheet.getRange(1,i).getValue());
@@ -271,7 +307,7 @@ function differenceCalc() {
   //Add column and do match
   sheet.insertColumnAfter(sheet.getLastColumn());
   sheet.getRange(1,sheet.getLastColumn()+1).setValue(Headers.STDTIME);
-  for(let i=2; i<sheet.getMaxRows(); i++) {
+  for(let i=2; i<=sheet.getMaxRows(); i++) {
     let estA1 = sheet.getRange(i,estIndex).getA1Notation();
     let rollA1 = sheet.getRange(i,rollIndex).getA1Notation();
     let difference = '=('+estA1+'-'+rollA1+')';
@@ -279,7 +315,7 @@ function differenceCalc() {
   }
 }
 
-function correctingFactor() {
+function calcCorrectingFactor() {
   //for each dev, sum the formatted hours
   //Compare to the Number of working days in current month?
 }
